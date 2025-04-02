de49ae358ddbddf083eb68e0b0ce3e46054727c2
nazam-007 <145904157+nazam-007@users.noreply.github.com>
2025-04-02 19:48:52 +0530

    clerk added

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,4 +1,8 @@
 
 
 //API controller function to manage clerk user with database
-// http://localhost:4000/api/user/webhooks 
+// http://localhost:4000/api/user/webhooks
+
+const clerkWebhooks=async(req,res)=>{
+    
+}
