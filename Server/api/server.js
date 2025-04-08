46ec3321d6c9cb7eca4297c198e57d0666c16934
nazam-007 <145904157+nazam-007@users.noreply.github.com>
2025-04-08 08:38:59 +0530

    Updated vercel.json updated

--- Server/api/server.js
+++ Server/api/server.js
@@ -1,7 +1,8 @@
 import 'dotenv/config';
 import express, { request } from 'express'
 import cors from 'cors'
-import connectDB from './config/mongodb.js';
+import connectDB from '../config/mongodb.js';
+import serverless from 'serverless-http';
 // import connectDB from './config/mongodb.js';
 // import { EventEmitter } from "events";
 // EventEmitter.defaultMaxListeners = 15; // Set the limit higher
@@ -11,9 +12,9 @@ import connectDB from './config/mongodb.js';
 // import dotenv from "dotenv";
 // dotenv.config();
 
-const PORT=process.env.PORT || 4000;
+//const PORT=process.env.PORT || 4000;
 const app=express();
-await connectDB()
+//await connectDB()
 
 //Middleware 
 app.use(express.json())  //whenever we get any request on this server it will parse using json
@@ -22,4 +23,12 @@ app.use(cors())  //tjhis will help to run on diffrednt server on backen server
 //API ROUTES
 app.get('/',(req,res)=>(res.send("API is Working")));
 
-app.listen(PORT,()=>console.log(`Server is running on port ${PORT} successfully`))
+//app.listen(PORT,()=>console.log(`Server is running on port ${PORT} successfully`))
+
+connectDB().then(() => {
+    console.log("MongoDB connected");
+  }).catch((err) => {
+    console.error("DB connection failed:", err);
+  });
+  
+  export const handler = serverless(app);
